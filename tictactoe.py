524af9815a1cd8c5c54250bfc5cfed5c454b24c6
Don Roche <donrochemedia@gmail.com>
2021-03-24 11:51:24 +0000

    feat/add marker

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,7 @@
-markers = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#Stores game markers for X an O's. Default is empty.
+
+markers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+gameOn = True
 
 def print_board():
     print(markers[0], " ¦ ", markers[1], " ¦ ", markers[2])
@@ -7,4 +10,7 @@ def print_board():
     print("-------------")
     print(markers[6], " ¦ ", markers[7], " ¦ ", markers[8])
 
-print_board()
+while gameOn:
+    print_board()
+    newPossition= int(input("Place X marker in possition 1-2-3-4-5-6-7-8-9? "))
+    markers[newPossition-1] = "X"
