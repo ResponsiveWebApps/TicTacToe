2af5b94509d5812f1894fc9bf26a86f326676379
Don Roche <donrochemedia@gmail.com>
2021-03-24 12:15:17 +0000

    feat/random marker

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import random
+
 #Stores game markers for X an O's. Default is empty.
 
 markers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -10,7 +12,20 @@ def print_board():
     print("-------------")
     print(markers[6], " ¦ ", markers[7], " ¦ ", markers[8])
 
+def ai_Marker():
+    aiMarker = random.randrange(0,8)
+
+    while markers[aiMarker] == "X" or markers[aiMarker] == "O":
+        aiMarker = random.randrange(0,8)
+
+    markers[aiMarker] = "O"
+
 while gameOn:
     print_board()
-    newPossition= int(input("Place X marker in possition 1-2-3-4-5-6-7-8-9? "))
+    newPossition= int(input("Place X marker in possition 1-2-3-4-5-6-7-8-9? 0 to exit "))
+
+    if newPossition == 0:
+        break
+
     markers[newPossition-1] = "X"
+    ai_Marker()
