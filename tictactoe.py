11a63c2c48756dd43d977d9c6e963108f791de60
Don Roche <donrochemedia@gmail.com>
2021-03-24 11:34:11 +0000

    feat/board

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+markers = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def print_board():
+    print(markers[0], " ¦ ", markers[1], " ¦ ", markers[2])
+    print("-------------")
+    print(markers[3], " ¦ ", markers[4], " ¦ ", markers[5])
+    print("-------------")
+    print(markers[6], " ¦ ", markers[7], " ¦ ", markers[8])
+
+print_board()
