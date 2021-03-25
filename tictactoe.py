9b53b3b0e0b8ec4406ea56d33593b0beb0203a2c
Don Roche <donrochemedia@gmail.com>
2021-03-25 18:32:15 +0000

    feat/scores

--- tictactoe.py
+++ tictactoe.py
@@ -2,10 +2,14 @@ import random
 
 #Stores game markers for X an O's. Default is empty.
 
-default_markers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-markers = default_markers
+markers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 gameOn = True
+
+#Monitors who won. 
+
 player = "Human"
+human_score = 0
+ai_score = 0
 
 def print_board():
     print(markers[0], " ¦ ", markers[1], " ¦ ", markers[2])
@@ -14,9 +18,9 @@ def print_board():
     print("-------------")
     print(markers[6], " ¦ ", markers[7], " ¦ ", markers[8])
 
-#Checks win conditions for game
+#Checks win conditions for game.
 
-def game_won():
+def game_won(human_score, ai_score):
     if markers[0] == markers[1] == markers[2] or \
        markers[3] == markers[4] == markers[5] or \
        markers[6] == markers[7] == markers[8] or \
@@ -26,11 +30,16 @@ def game_won():
        markers[0] == markers[4] == markers[8] or \
        markers[2] == markers[4] == markers[6]:
 
-        print("Game won!")
+        if player == "Human":
+            human_score += 1
+        else:
+            ai_score += 1
+
+        print(player, "won!")
+        print("Human:", human_score, "AI:", ai_score)
         print("\n")
         reset_game()
-        
-        
+                
 def reset_game():
     markers[0] = "1" 
     markers[1] = "2" 
@@ -42,8 +51,7 @@ def reset_game():
     markers[7] = "8"
     markers[8] = "9"
 
-
-#AI places marker
+#AI places marker.
 def ai_marker():
     aiMarker = random.randrange(0,8)
 
@@ -52,20 +60,20 @@ def ai_marker():
 
     markers[aiMarker] = "O"
 
-#Runs game
+#Runs game.
 while gameOn:
     print_board()
     print("\n")
     newPossition = int(input("Place X marker in possition 1-2-3-4-5-6-7-8-9?" + "\n" + "0 to exit "))
     print("\n")
 
-    #Exits game
+    #Exits game.
     if newPossition == 0:
         break
 
     markers[newPossition-1] = "X"
     player = "Human"
-    game_won()
+    game_won(human_score, ai_score)
     ai_marker()
     player = "AI"
-    game_won()
+    game_won(human_score, ai_score)
