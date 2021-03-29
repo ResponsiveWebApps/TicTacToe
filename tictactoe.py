197483a696af5fe6b87614a8e95ab40774e0be0d
Don Roche <donrochemedia@gmail.com>
2021-03-29 10:38:15 +0000

    minimax

--- tictactoe.py
+++ tictactoe.py
@@ -61,9 +61,44 @@ def empty_cells(state):
     return cells
 
 #Checks if move is vlid in the empty-cells.
- 
+
 def valid_move(x, y):
     if [x, y] in empty_cells(board):
         return True
     else:
         return False
+
+def set_move(x, y, player):
+    if valid_move(x, y):
+        board[x][y] = player
+        return True
+    else:
+        return False
+
+#Calculate the minimax for optimal move
+
+def minimax(state, depth, player):
+    if player == COMP:
+        best = [-1, -1, -infinity]
+    else:
+        best = [-1, -1, +infinity]
+
+    if depth == 0 or game_won(state):
+        score = evaluate(state)
+        return [-1, -1, score]
+
+    for cell in empty_cells(state):
+        x, y = cell[0], cell[1]
+        state[x][y] = player
+        score = minimax(state, depth - 1, -player)
+        state[x][y] = 0
+        score[0], score[1] = x, y
+
+        if player == ai:
+            if score[2] > best[2]:
+                best = score  # max value
+        else:
+            if score[2] < best[2]:
+                best = score  # min value
+
+    return best
